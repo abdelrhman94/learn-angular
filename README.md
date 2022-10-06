3777f4aaa967024fd6c2e7a4fa1cc428c41b930d
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2022-10-06 19:53:55 +0200

    Introduction to components
    create class component with code support the viwe
    use decorator to define the component metadata
    import what I need in app module

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 1. install angular cli globally
 2. have a quick overview on the folder in angular application
+
+# Introduction to components
+
+1. what is component?
+2. creating class component
+3. defining metadata with a decorator
+4. bootstrapping app component
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'learn-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learn-angular');
+    expect(app.pageTitle).toEqual('learn-angular');
   });
 
   it('should render title', () => {
